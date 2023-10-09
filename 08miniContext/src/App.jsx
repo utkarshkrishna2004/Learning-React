1671f2a0ee31c4a5a9f7e07359e30679f98c4d4a
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-09 22:36:43 +0530

    Deeper dive in react starts today!

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,13 +1,18 @@
 
 import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   
 
   return (
-    <>
-      <h1>React with Context API</h1>
-    </>
+    <UserContextProvider>
+      <h1>Learning Context API</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
