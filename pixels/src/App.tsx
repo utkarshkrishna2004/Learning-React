87c8d0ca86d91227e8996887c0cc9aeda4bd3ba6
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-27 19:48:33 +0530

    React Routing structure established.

--- pixels/src/App.tsx
+++ pixels/src/App.tsx
@@ -1,9 +1,31 @@
-import './globals.css';
+import { Routes, Route } from "react-router-dom";
+
+import "./globals.css";
+
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
-  return (
-    <h1 className='text-3xl font-bold underline'>Hello Pixels!</h1>
-  )
-}
+   return (
+      <main className="flex h-screen">
+         <Routes>
+            {/* public routes */}
+            <Route element={<AuthLayout />}>
+               <Route path="/sign-in" element={<SigninForm />} />
+               <Route path="/sign-up" element={<SignupForm />} />
+            </Route>
 
-export default App
+
+            {/* private routes */}
+            <Route element={<RootLayout />}>
+               <Route index element={<Home />} />
+            </Route>
+         </Routes>
+      </main>
+   );
+};
+
+export default App;
